4b2f577e584b207c266aff123be307773a6aa1ac
moritasoshi <soshi.morita@rakus-partners.co.jp>
2020-09-10 10:03:38 +0900

    Update the Board view

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,46 +1,55 @@
 <template>
-  <v-container>
-    <h2>{{ board.title }}</h2>
+  <v-app>
+    <h2>{{ board.board_name }}</h2>
 
-    <v-container class="d-flex justify-space-between">
+    <v-container class="d-flex">
       <v-card
-        min-width="300"
-        max-width="400"
-        class="mx-auto"
+        max-width="260"
+        class="mx-1"
         v-for="(tile, index) in board.tiles"
         :key="index"
       >
         <v-app-bar dark color="cyan" dense>
           <v-toolbar-title>{{ tile.name }}</v-toolbar-title>
           <v-spacer></v-spacer>
-          <v-btn icon @click="addCard">
+          <!-- <v-btn icon @click="addCard">
             <v-icon>mdi-plus</v-icon>
-          </v-btn>
+          </v-btn> -->
         </v-app-bar>
         <v-container>
           <draggable group="all-tasks" :list="tile.cards">
-            <v-col v-for="(card, i) in tile.cards" :key="i" cols="12">
-              <v-card width="250" height="70" hover link>
-                <div class="d-flex flex-no-wrap justify-space-between">
-                  <div>
-                    <v-card-text v-text="card.name"></v-card-text>
-                  </div>
-                  <div>
-                    <v-btn icon>
-                      <v-icon>mdi-delete</v-icon>
-                    </v-btn>
-                    <v-btn icon>
-                      <v-icon>mdi-pencil-outline</v-icon>
-                    </v-btn>
-                  </div>
-                </div>
-              </v-card>
-            </v-col>
+            <v-card
+              v-for="(card, i) in tile.cards"
+              :key="i"
+              width="220"
+              hover
+              link
+              class="d-flex my-card"
+            >
+              <v-card-text color="black" v-text="card.name"></v-card-text>
+              <v-card-actions>
+                <v-btn icon>
+                  <v-icon>mdi-delete</v-icon>
+                </v-btn>
+              </v-card-actions>
+            </v-card>
           </draggable>
+          <v-btn text color="grey" @click="addCard">
+            <v-icon>mdi-plus</v-icon>
+            カードを追加
+          </v-btn>
         </v-container>
       </v-card>
+      <v-card width="260" height="50" outlined>
+        <v-app-bar dark color="white" dense flat>
+          <v-btn text @click="addTile" color="cyan">
+            <v-icon>mdi-plus</v-icon>リストを追加
+          </v-btn>
+          <v-spacer></v-spacer>
+        </v-app-bar>
+      </v-card>
     </v-container>
-  </v-container>
+  </v-app>
 </template>
 
 <script>
@@ -66,7 +75,15 @@ export default {
       );
     },
   },
+  methods: {
+    addCard() {},
+    addTile() {},
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.my-card {
+  margin: 10px 0;
+}
+</style>
